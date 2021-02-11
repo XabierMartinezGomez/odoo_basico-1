9aaec4c80a11c1809a2e4cb2add4b745180ac4cb
antonio <antoniocfrv@gmail.com>
2021-02-11 12:18:42 +0100

    Versión 11/2/21. Localize

--- models/informacion.py
+++ models/informacion.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import Warning
+import locale
+import pytz
 
 class informacion(models.Model):
      _name = 'odoo_basico.informacion' # Será o nome da táboa
@@ -30,6 +32,15 @@ class informacion(models.Model):
      moeda_euro_id = fields.Many2one('res.currency',default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")],limit=1))
      gasto_en_euros = fields.Monetary("Gasto en Euros", 'moeda_euro_id')
 
+     data = fields.Date(string="Data", default=lambda self: fields.Date.today())
+     data_hora = fields.Datetime(string="Data e Hora", default=lambda self: fields.Datetime.now())
+     mes_castelan = fields.Char(compute="_mes_castelan", size=15, string="Mes en castelán", store=True)
+     mes_galego = fields.Char(compute="_mes_galego", size=15,string="Mes en galego", store=True)
+     hora_utc = fields.Char(compute="_hora_utc", string="Hora UTC", size=15, store=True)
+     # hora_timezone_usuario = fields.Char(compute="_hora_timezone_usuario", string="Hora Timezone do Usuario", size=15,
+     #                                     store=True)
+     hora_actual = fields.Char(compute="_hora_actual", string="Hora Actual", size=15, store=True)
+
      @api.depends('alto_en_cms','longo_en_cms','ancho_en_cms')
      def _volume(self):
           for rexistro in self:
@@ -45,3 +56,67 @@ class informacion(models.Model):
 
      def _cambia_campo_sexo(self, rexistro):
           rexistro.sexo_traducido = "Hombre"
+
+     def ver_contexto(self):  # Este método é chamado dende un botón de informacion.xml
+         for rexistro in self:  # Ao usar warning temos que importar a libreria from odoo.exceptions import Warning
+             raise Warning('Contexto: %s' % rexistro.env.context)  # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
+         return True
+
+     @api.depends('data_hora')
+     def _mes_castelan(self):
+         # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+         # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+         # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+         locale.setlocale(locale.LC_TIME, 'es_ES.utf8')  # Para GNU/Linux
+         # locale.setlocale(locale.LC_TIME, 'Spanish_Spain.1252')  # Para Windows
+         for rexistro in self:
+             rexistro.mes_castelan = rexistro.data_hora.strftime("%B")  # strftime https://strftime.org/
+
+     @api.depends('data_hora')
+     def _mes_galego(self):
+         # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+         # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+         # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+         locale.setlocale(locale.LC_TIME, 'gl_ES.utf8')  # Para GNU/Linux
+         # locale.setlocale(locale.LC_TIME, 'Galician_Spain.1252')  # Para Windows
+         for rexistro in self:
+             rexistro.mes_galego = rexistro.data_hora.strftime("%B")
+         locale.setlocale(locale.LC_TIME, 'es_ES.utf8')  # Para GNU/Linux
+         # locale.setlocale(locale.LC_TIME, 'Spanish_Spain.1252')  # Para Windows
+
+     @api.depends('data_hora')
+     def _hora_utc(self):
+        for rexistro in self:  # A hora se almacena na BD en horario UTC (2 horas menos no verán, 1 hora menos no inverno)
+            rexistro.hora_utc = rexistro.data_hora.strftime("%H:%M:%S")
+
+     def actualiza_hora_actual_UTC(self):  # Esta función é chamada dende un boton de informacion.xml e dende _hora_actual
+         for rexistro in self:
+             rexistro.hora_actual = fields.Datetime.now().strftime("%H:%M:%S")
+         # Grava a hora en UTC, se quixesemos poderiamos usar a función  _convirte_data_hora_de_utc_a_timezone_do_usuario
+
+     @api.depends('data_hora')
+     def _hora_actual(self):
+         for rexistro in self:
+             rexistro.actualiza_hora_actual_UTC()
+         # Esta función será chamada dende a función actualiza_hora_timezone_usuario_dende_boton_e_apidepends e
+         #  dende pedido.py (Cando insertamos os valores do template self.env.user.tz non ten o timezone do usuario por iso se carga coa hora UTC,
+         #  o botón en pedido.py é para actualizar todos os rexistros masivamente dende outro modelo)
+
+     def convirte_data_hora_de_utc_a_timezone_do_usuario(self,data_hora_utc_object):  # recibe a data hora en formato object
+         usuario_timezone = pytz.timezone(self.env.user.tz or 'UTC')  # obter a zona horaria do usuario. Ollo!!! nas preferencias do usuario ten que estar ben configurada a zona horaria
+         return pytz.UTC.localize(data_hora_utc_object).astimezone(usuario_timezone)  # hora co horario do usuario en formato object
+         # para usar  pytz temos que facer  import pytz
+
+     def actualiza_hora_timezone_usuario(self, obxeto_rexistro):
+         obxeto_rexistro.hora_timezone_usuario = self.convirte_data_hora_de_utc_a_timezone_do_usuario(
+             obxeto_rexistro.data_hora).strftime("%H:%M:%S")  # Convertimos a hora de UTC a hora do timezone do usuario
+
+     def actualiza_hora_timezone_usuario_dende_boton_e_apidepends(self):  # Esta función é chamada dende un boton de informacion.xml e dende @api.depends _hora_timezone_usuario
+         self.actualiza_hora_timezone_usuario(self)  # leva self como parametro por que actualiza_hora_timezone_usuario ten 2 parametros
+         # porque usamos tamén actualiza_hora_timezone_usuario dende outro modelo (pedido.py) e lle pasamos como parámetro o obxeto_rexistro
+
+     @api.depends('data_hora')
+     def _hora_timezone_usuario(self):
+         for rexistro in self:
+             rexistro.actualiza_hora_timezone_usuario_dende_boton_e_apidepends()
+
--- models/pedido.py
+++ models/pedido.py
@@ -15,3 +15,20 @@ class pedido(models.Model):
           informacion_ids = self.env['odoo_basico.informacion'].search([('autorizado', '=', False)])
           for rexistro in informacion_ids:
                self.env['odoo_basico.informacion']._cambia_campo_sexo(rexistro)
+
+     def creaRexistroInformacion(self):
+         creado_id = self.env['odoo_basico.informacion'].create({'name': 'Creado dende pedido'})
+         creado_id.descripcion = "Creado dende o modelo pedido"
+         creado_id.autorizado = False
+
+     def actualizaRexistroInformacion(self):
+          informacion_id = self.env['odoo_basico.informacion'].search([('name', '=', 'Creado dende pedido')])
+          if informacion_id:
+               informacion_id.name = "Actualizado ..."
+               informacion_id.descripcion = "Actualizado dende o modelo pedido"
+               informacion_id.sexo_traducido = "Mujer"
+
+     def actualizadorHoraTimezone(self):
+          informacion_ids = self.env['odoo_basico.informacion'].search([])
+          for rexistro in informacion_ids:
+               self.env['odoo_basico.informacion'].actualiza_hora_timezone_usuario(rexistro)
